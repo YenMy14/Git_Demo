c74529411df80af6b09a62f66fbc1a789913c190
Ly Mai <maithily14122001@gmail.com>
2022-09-06 21:21:14 +0700

    Add Class2.cs file

---
+++ Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mai Thi Ly !");
+        }
+    }
+}
